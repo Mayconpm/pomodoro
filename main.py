357a97320ddbd3de2fa06cac2ea42ec90872e18a
maycon.marciano <pm.maycon@gmail.com>
2023-09-09 22:12:51 -0300

    Refactor GUI layout; add timer label, checkmark label, start button, and reset button for improved functionality.
    - Refactored GUI layout by adding timer label, checkmark label, start button, and reset button.
    - Replaced `tomato_img` variable with `canvas_tomato_img` for consistency.
    - Aligned canvas to column 1, row 2 and adjusted its position accordingly.
    - Aligned timer label to column 1, row 1.
    - Aligned checkmark label to column 1, row 4.
    - Aligned start button to column 0, row 3.
    - Aligned reset button to column 2, row 3.
    - Updated code for improved functionality and user experience.

--- main.py
+++ main.py
@@ -22,10 +22,22 @@ window.title("Pomodoro")
 window.config(padx=50, pady=50, background=YELLOW)
 
 canvas = tk.Canvas(width=200, height=224, background=YELLOW, highlightthickness=0)
-tomato_img = tk.PhotoImage(file="tomato.png")
-canvas.create_image(100, 112, image=tomato_img)
+canvas_tomato_img = tk.PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=canvas_tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas
-canvas.pack()
+canvas.grid(column=1, row=2)
+
+timer_label = tk.Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+timer_label.grid(column=1, row=1)
+
+checkmark_label = tk.Label(text=" ✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 12, "bold"))
+checkmark_label.grid(column=1, row=4)
+
+start_button = tk.Button(text="Start", font=(FONT_NAME, 12, "bold"))
+start_button.grid(column=0, row=3)
+
+reset_button = tk.Button(text="Reset", font=(FONT_NAME, 12, "bold"))
+reset_button.grid(column=2, row=3)
+
 
 window.mainloop()
